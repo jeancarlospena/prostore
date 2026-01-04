7836b6bba7a5c5f83357103b7150ed634b722233
Carlos <jcpacc6@outlook.com>
2026-01-04 18:04:34 -0500

    updated to 16

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,20 @@
-export {auth as middleware} from '@/auth'
+// export {auth as middleware} from '@/auth'
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function middleware(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-my-favorite-show", "Breaking Bad");
+
+
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
+
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
